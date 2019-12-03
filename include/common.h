9374ddff8ad831419c65c6362fa8ab85a65200e2
Joe Hershberger <joe.hershberger@ni.com>
2019-12-03 13:11:22 -0600

    Revert "common: Move env_get_ip() to net.h"
    This reverts commit 8d5babb45a051efb9ce4c9e06a693d72ee6cba5a.

--- include/common.h
+++ include/common.h
@@ -152,6 +152,10 @@ int get_serial_clock(void);
 
 /* lib/net_utils.c */
 #include <net.h>
+static inline struct in_addr env_get_ip(char *var)
+{
+	return string_to_ip(env_get(var));
+}
 
 #include <bootstage.h>
 
